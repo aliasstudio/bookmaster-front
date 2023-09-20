bad81f94bc15f4b82233ee0701ca6db2e4614354
aliasstudio <ownerofyourcode@gmail.com>
2023-09-20 12:25:12 +0500

    build: добавлен модуль для уведомлений
    Refs #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgxsModule } from "@ngxs/store";
 import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 import { AppState } from "./store/app.state";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { GlobalConfig, ToastrModule } from "ngx-toastr";
+
+const ToastrOptions: Partial<GlobalConfig> = {
+  timeOut: 3000,
+  progressBar: true,
+  progressAnimation: 'decreasing',
+  tapToDismiss: true,
+  positionClass: 'toast-top-right',
+  maxOpened: 3,
+}
 
 @NgModule({
   declarations: [
@@ -16,11 +27,13 @@ import { AppState } from "./store/app.state";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
     NgxsModule.forRoot([AppState]),
     NgxsLoggerPluginModule.forRoot(),
+    ToastrModule.forRoot(ToastrOptions),
   ],
   providers: [
     {
