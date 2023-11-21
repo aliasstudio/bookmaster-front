54d405c11493059198a450ff6add65d94c294e68
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2023-11-21 22:39:28 +0500

    feat(href pipe): добавлен пайп для ссылок

---
+++ src/app/shared/pipes/href.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'href',
+})
+export class HrefPipe implements PipeTransform {
+  transform(value: string, content?: string): string {
+    const link = value?.match(
+      '(http|https):\\/\\/([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:\\/~+#-]*[\\w@?^=%&\\/~+#-])',
+    )?.[0];
+    return link ? `<a href="${link}">${content || 'Перейти'}</a>` : value;
+  }
+}
