0f05ed3fc5c6d1cea46903f537d7901a50bb109b
IldarZar <ildarzaripov2002@yandex.ru>
2023-12-07 00:05:43 +0500

    feat(book-return): добавлена страница выдачи книг3
    Refs #13

--- src/app/book-return/book-return-page/book-return-page.component.ts
+++ src/app/book-return/book-return-page/book-return-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   MatDatatableControlComponent
 } from "@app/shared/components/mat-datatable-control/mat-datatable-control.component";
@@ -10,13 +10,31 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./book-return-page.component.scss'],
   providers: [MatDatatableControlComponent],
 })
-export class BookReturnPageComponent {
+export class BookReturnPageComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
   ) {}
 
-  public findClient(customerId: string): void {
+  ngOnInit() {
+    // this.http.get('issue?filter=Болконский Андрей Николаевич').subscribe(res => console.log(res));
+    this.http.get('issue/actual').subscribe(res => console.log(res));
+    // this.http.post('issue', {
+    //   "dateOfIssue": "2023-12-01",
+    //   "returnUntil": "2023-12-20",
+    //   "dateOfReturn": "2023-12-18",
+    //   "customer": {
+    //     "id": "C1000",
+    //   },
+    //   "book": {
+    //     "uuid": "B1056",
+    //   }
+    // }).subscribe();
+  }
+
+  public findCustomer(customerId: string): void {
+
+
     this.http.get(`customer/${customerId}`).subscribe(res => console.log(res));
   }
 }
