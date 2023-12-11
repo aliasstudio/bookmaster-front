599d5751017f491be1c13f4d89d494bcc178028e
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2023-12-11 22:30:25 +0500

    feat(book-turnover): пайп расчета задержки возврата книги

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -4,7 +4,6 @@ import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsPageComponent } from './reports-page/reports-page.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { ReportsTurnoverPageComponent } from './reports-turnover-page/reports-turnover-page.component';
-import { ReportsReturnUntilColumnComponent } from '@app/reports/reports-turnover-page/reports-return-until-column/reports-return-until-column.component';
 import { BookHistoryPageComponent } from '@app/reports/book-history-page/book-history-page.component';
 import { BookHistoryGridPageComponent } from '@app/reports/book-history-grid-page/book-history-grid-page.component';
 
@@ -12,7 +11,6 @@ import { BookHistoryGridPageComponent } from '@app/reports/book-history-grid-pag
   declarations: [
     ReportsPageComponent,
     ReportsTurnoverPageComponent,
-    ReportsReturnUntilColumnComponent,
     BookHistoryGridPageComponent,
     BookHistoryPageComponent,
   ],
